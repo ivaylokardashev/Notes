f248a57d3e9ce788592eee109e7fcd26024fc729
KirukaFukamo <ivaylo.kardashev@gmail.com>
2025-05-13 14:33:37 +0300

    Add views for creating Notes

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,10 +2,36 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import generics
-from .serializers import UserSerializer
+from .serializers import UserSerializer, NoteSerializer
+from .models import Note
 
 
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all() # Check if there is similiar user
     serializer_class = UserSerializer # What data to be accept
     permission_classes = [AllowAny] # Give permission to all
+
+class NoteListCreate(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classess = [IsAuthenticated] # Just for try use AllowAny to see is it class work
+
+    # Show all notes of the authenticated user
+    def get_queryset(self):
+        user = self.request.user    # ask which is the user
+        return Note.objects.filter(author=user)     # filter all Note objects by user
+    
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:
+            print(serializer.errors)
+
+# This class Destroy the Note because is child of DestroyAPIView
+class NoteDelete(generics.DestroyAPIView):
+    serializer_class = NoteSerializer
+    permission_classess = [IsAuthenticated]
+
+    # Show all notes of the authenticated user
+    def get_queryset(self):
+        user = self.request.user    # ask which is the user
+        return Note.objects.filter(author=user)     # filter all Note objects by user
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from api.views import CreateUserView
 # Prebuild views for creating JWT (access + refresh) token and preview for refreshing token
 # Helps after creating user to assign token to them
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 # Links root to views
 # When go to specific root we call specific view
@@ -14,5 +14,5 @@ urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("api-auth/", include("rest_framework.urls")),
-    
+    path("api/", include("api.urls")),
 ]
