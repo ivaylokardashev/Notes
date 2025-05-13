ef2bf7038632588bab4ed5eb77025912f0862b13
KirukaFukamo <ivaylo.kardashev@gmail.com>
2025-05-13 14:35:34 +0300

    Making pages and routes
    Create routes for base pages

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import react from "react"
+import {BrowserRouter, Routes, Route, Navigation} from "react-router-dom"   //
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
+import ProtectedRoute from "./components/ProtectedRoute"
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
