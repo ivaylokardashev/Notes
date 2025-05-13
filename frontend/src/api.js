93ec6e875f7f3ab27fe2017b487a2c2350a88788
KirukaFukamo <ivaylo.kardashev@gmail.com>
2025-05-13 21:44:11 +0300

    Creating the api and App.jsx

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,23 +1,21 @@
-import axios from "axios"
-import { ACCESS_TOKEN } from "./constants" // This is the our token
+import axios from "axios";
+import { ACCESS_TOKEN } from "./constants";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
-})
+  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
+});
 
-// This is for pass JWT token
 api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem(ACCESS_TOKEN);
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`
-        }
-        return config
-    },
-    (error) => {
-        return Promise.reject(error)
+  (config) => {
+    const token = localStorage.getItem(ACCESS_TOKEN);
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
-)
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
-// We will use this object instead default axios
-export default api
+export default api;
