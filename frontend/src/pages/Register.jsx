2d61b3842255f52ad5170e06d2db3d686c01e0a2
KirukaFukamo <ivaylo.kardashev@gmail.com>
2025-05-13 21:43:39 +0300

    Creating pages for the frontend

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,9 @@
+import Form from "../components/Form"
+import { useNavigate } from "react-router-dom";
+
+
 function Register() {
-    return <div>Register</div>
+    return <Form route="/api/user/register/" method="register" />
 }
 
 export default Register
