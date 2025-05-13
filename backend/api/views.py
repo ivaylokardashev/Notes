2f6f190728a7c2bbfd03dddeae8dafa8c9479072
KirukaFukamo <ivaylo.kardashev@gmail.com>
2025-05-13 21:45:44 +0300

    Packages

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,37 +1,36 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import generics
 from .serializers import UserSerializer, NoteSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Note
 
 
-class CreateUserView(generics.CreateAPIView):
-    queryset = User.objects.all() # Check if there is similiar user
-    serializer_class = UserSerializer # What data to be accept
-    permission_classes = [AllowAny] # Give permission to all
-
 class NoteListCreate(generics.ListCreateAPIView):
     serializer_class = NoteSerializer
-    permission_classess = [IsAuthenticated] # Just for try use AllowAny to see is it class work
+    permission_classes = [IsAuthenticated]
 
-    # Show all notes of the authenticated user
     def get_queryset(self):
-        user = self.request.user    # ask which is the user
-        return Note.objects.filter(author=user)     # filter all Note objects by user
-    
+        user = self.request.user
+        return Note.objects.filter(author=user)
+
     def perform_create(self, serializer):
         if serializer.is_valid():
             serializer.save(author=self.request.user)
         else:
             print(serializer.errors)
 
-# This class Destroy the Note because is child of DestroyAPIView
+
 class NoteDelete(generics.DestroyAPIView):
     serializer_class = NoteSerializer
-    permission_classess = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
-    # Show all notes of the authenticated user
     def get_queryset(self):
-        user = self.request.user    # ask which is the user
-        return Note.objects.filter(author=user)     # filter all Note objects by user
+        user = self.request.user
+        return Note.objects.filter(author=user)
+
+
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -64,6 +64,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -71,7 +72,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -146,5 +146,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGIN = True
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_ALL_CREDENTIALS = True
